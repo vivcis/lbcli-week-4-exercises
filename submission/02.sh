e2b841f2391361bfc5db14bcc93068e12a99efc2
vivcis <ceciliaorji.co@gmail.com>
2026-04-07 23:45:10 +0100

    fix script 2

--- submission/02.sh
+++ submission/02.sh
@@ -7,8 +7,11 @@
 #!/bin/bash
 
 transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
-txid=$(bitcoin-cli decoderawtransaction "$transaction" | jq -r '.txid')
-sequence=2016
+
+txid=$(bitcoin-cli -regtest decoderawtransaction "$transaction" | jq -r '.txid')
+locktime=2041
+
+sequence=4294967293
 
 inputs=$(jq -nc --arg txid "$txid" --argjson seq "$sequence" \
   '[{"txid":$txid,"vout":0,"sequence":$seq},
@@ -16,5 +19,6 @@ inputs=$(jq -nc --arg txid "$txid" --argjson seq "$sequence" \
 
 outputs='[{"2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP":0.20000000}]'
 
-raw_tx=$(bitcoin-cli createrawtransaction "$inputs" "$outputs")
+raw_tx=$(bitcoin-cli -regtest createrawtransaction "$inputs" "$outputs" "$locktime")
+
 echo "$raw_tx"
